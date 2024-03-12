1aea2a88ce8bfb993c4434fa66e5a4f3b329ee77
Your Name <you@example.com>
2024-03-12 10:25:56 +0000

    api work to this point

---
+++ data.js
@@ -0,0 +1,16 @@
+let data = [
+  {
+    id: 1,
+    name: "item 1",
+  },
+  {
+    id: 2,
+    name: "item 2",
+  },
+  {
+    id: 3,
+    name: "item 3",
+  },
+];
+
+export default data;
---
+++ server.js
@@ -0,0 +1,35 @@
+import express from "express";
+import data from "./data.js";
+import bodyParser from "body-parser";
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
+
+app.use(bodyParser.json());
+
+app.get("/items", (req, res) => {
+  res.send(data);
+});
+
+app.post("/items", (req, res) => {
+  const newItem = {
+    id: Date.now(),
+    name: req.body.name,
+  };
+  data.push(newItem);
+  res.send(newItem);
+});
+
+app.put("/items/:id", (req, res) => {
+  const item = data.find((i) => i.id === parseInt(req.params.id));
+
+  if (!item) {
+    res.status(404).send("Item not found");
+  } else {
+    item.name = req.body.name;
+    res.send(item);
+  }
+});
